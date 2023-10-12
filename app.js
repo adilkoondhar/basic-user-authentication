308f5c9a26e9f7d2d0d5af7939be3bceec3ac317
Adil <146661723+adilkoondhar@users.noreply.github.com>
2023-10-12 23:15:15 +0500

    Add basic user authentication

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
+const mongoose = require('mongoose')
 
 const app = express()
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
+
+mongoose.connect('mongodb://localhost:27017/userDB')
+
+const User = mongoose.model('User', {email: String, password: String})
 
 app.route('/')
 .get((req, res) => {
@@ -17,10 +22,27 @@ app.route('/login')
 .get((req, res) => {
     res.render('login')
 })
+.post((req, res) => {
+    User.findOne({'email': req.body.email} )
+      .then((user) => {
+        if(user.password == req.body.password) {
+            res.render('secrets')
+        } else {
+            console.log('User not found')
+        }
+      })
+  })
 
 app.route('/register')
 .get((req, res) => {
     res.render('register')
+})
+.post((req, res) => {
+    const newUser = new User(req.body)
+
+    newUser.save().then(() => {
+        res.render('secrets')
+    })
 })
 
 app.route('/secrets')
